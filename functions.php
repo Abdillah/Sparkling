b7467df2b63d8e03096ccef9582f53447a2515a7
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-05-27 23:13:39 +0700

    Add logger function for debugging

--- functions.php
+++ functions.php
@@ -5,6 +5,16 @@
  * @package sparkling
  */
 
+if ( ! function_exists('write_log')) :
+    function write_log ( $log )  {
+        if ( is_array( $log ) || is_object( $log ) ) {
+            error_log( print_r( $log, true ) );
+        } else {
+            error_log( $log );
+        }
+    }
+endif;
+
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
